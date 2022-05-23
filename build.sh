2bec4fb9af6603f21f59967710953d80a43184c4
Nicolas Lacombe <nicolas.m.lacombe@gmail.com>
2022-05-23 14:24:26 -0400

    added base mkosi arch project

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+main() {
+	printSystemInfo 2>&1 > host-system-info.log
+	sudo mkosi 2>&1 | tee ./build.log
+	printCommandAndOutput "ls -lh ./mkosi.output/"
+}
+
+printSystemInfo() {
+	printCommandAndOutput "uname -a"
+	printCommandAndOutput "cat /etc/os-release"
+	printCommandAndOutput "sudo pacman -Qi mkosi"
+}
+
+printCommandAndOutput() {
+	local command="$1"
+
+	echo "$ $command"
+	$command
+}
+
+main "$@"
